02cb2a1b5ff0fa27fe382659eaf686a54ae45a27
dwiki1968 <dwiki1968@gmail.com>
2022-06-12 21:59:07 +0700

    problem promise output solved

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const XXI = await promiseTheaterIXX(); //data berupa array of object
+    const CGV = await promiseTheaterVGC(); //data berupa array of object
+    //concat data kedua data teater
+    const allData = XXI.concat(CGV);
+    //menfilter data yang diinginkan berdasarkan emosi
+    const result = allData.filter((e) => e.hasil === emosi).length;
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
